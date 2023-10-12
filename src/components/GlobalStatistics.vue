ce828ffbffb605670d4f73c4eba847333017e8eb
magnetenstad <tenstad.magne@gmail.com>
2023-10-12 21:37:20 +0200

    Separate table data

--- src/components/GlobalStatistics.vue
+++ src/components/GlobalStatistics.vue
@@ -1,26 +1,61 @@
 <template>
   <br />
+  <h3>Min 301-Double Visits</h3>
   <table>
-    <thead>
-      <th>User</th>
-      <th>Min 301-Double Visits</th>
-      <th>Max RTC Streak</th>
-      <th>Min RTC Visits</th>
-    </thead>
     <tbody>
-      <tr v-for="userStat in statsStore.userStats">
+      <tr
+        v-for="userStat in sort(
+          statsStore.userStats,
+          'min301DoubleVisits',
+          Infinity
+        )"
+      >
         <td>{{ useUsersStore().getUser(userStat.userId)?.name }}</td>
         <td>{{ userStat.min301DoubleVisits }}</td>
-        <td>{{ userStat.maxRtcStreak }}</td>
+      </tr>
+    </tbody>
+  </table>
+
+  <h3>Min RTC Visits</h3>
+  <table>
+    <tbody>
+      <tr
+        v-for="userStat in sort(statsStore.userStats, 'minRtcVisits', Infinity)"
+      >
+        <td>{{ useUsersStore().getUser(userStat.userId)?.name }}</td>
         <td>{{ userStat.minRtcVisits }}</td>
+      </tr>
+    </tbody>
+  </table>
+
+  <h3>Max RTC Streak</h3>
+  <table>
+    <tbody>
+      <tr
+        v-for="userStat in sort(statsStore.userStats, 'maxRtcStreak', 0, false)"
+      >
+        <td>{{ useUsersStore().getUser(userStat.userId)?.name }}</td>
+        <td>{{ userStat.maxRtcStreak }}</td>
       </tr>
     </tbody>
   </table>
 </template>
 
 <script lang="ts" setup>
-import { useStatsStore } from '@/stores/stats'
+import { useStatsStore, UserStat } from '@/stores/stats'
 import { useUsersStore } from '@/stores/users'
 
 const statsStore = useStatsStore()
+
+const sort = (
+  userStats: UserStat[],
+  key: keyof Omit<UserStat, 'userId'>,
+  _default: number,
+  ascending = true
+) => {
+  const sorted = userStats.toSorted(
+    (a, b) => (a[key] ?? _default) - (b[key] ?? _default)
+  )
+  return ascending ? sorted : sorted.toReversed()
+}
 </script>
