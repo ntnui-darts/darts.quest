df7b6c1566ebf057c5c196aa8e6b8e0e2ca31564
Erlend Paulsen Skaaden <erlendskaaden@gmail.com>
2025-04-08 21:24:47 +0200

    add view for achievements

---
+++ src/components/AchievementStatistics.vue
@@ -0,0 +1,47 @@
+<template>
+  <h2>Achievements</h2>
+  <div class="col">
+    <div
+      v-for="achievement in achievementStore.achievementList"
+      class="row"
+      style="
+        background-color: #1a1a1a;
+        border-radius: 8px;
+        padding: 0.4em 0.6em;
+        justify-content: space-between;
+      "
+    >
+      <div class="col" style="gap: 0.5em">
+        <span style="font-size: large; font-weight: bold">{{
+          achievement.name
+        }}</span>
+        <span>{{ achievement.description }}</span>
+        <span v-if="achievement.unlocked && achievement.unlockedTime"
+          >Achieved
+          {{ new Date(achievement.unlockedTime).toDateString() }}</span
+        >
+      </div>
+      <svg
+        v-if="achievement.unlocked"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="3"
+        stroke="var(--c-green)"
+        width="60px"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="m4.5 12.75 6 6 9-13.5"
+        />
+      </svg>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useAchievementStore } from '@/stores/achievement'
+
+const achievementStore = useAchievementStore()
+</script>
