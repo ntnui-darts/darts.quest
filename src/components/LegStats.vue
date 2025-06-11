28fd868e52ed134803da4cf55f29812e23ec2547
Gregor Moe <moethegregor@gmail.com>
2025-06-11 14:29:04 +0200

    combined X01 and RTC DartboardChart and started to extend LegStats information

--- src/components/LegStats.vue
+++ src/components/LegStats.vue
@@ -7,6 +7,7 @@
         >{{ getGameDisplayName(leg) }} &emsp;</span
       >
       <span>{{ leg.visits.length }} visits</span>
+      <span> with {{ getOtherPlayerText }}</span>
     </p>
     <button @click="showChart" style="flex: 0">Chart</button>
   </div>
@@ -15,11 +16,12 @@
 <script lang="ts" setup>
 import { getGameDisplayName } from '@/games/games'
 import { useModalStore } from '@/stores/modal'
-import { Leg } from '@/types/game'
+import { DbGame, Leg } from '@/types/game'
 import DartboardChart from './DartboardChart.vue'
 
 const props = defineProps<{
   leg: Leg
+  game: DbGame
 }>()
 
 const showChart = () => {
