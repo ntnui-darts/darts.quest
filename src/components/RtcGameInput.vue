89910cf61fa5839e2bfd67e23e08e4bc1238a67e
Gregor Moe <moethegregor@gmail.com>
2025-12-21 12:07:58 +0100

    adds maxvisit option to rtc

--- src/components/RtcGameInput.vue
+++ src/components/RtcGameInput.vue
@@ -1,26 +1,36 @@
 <template>
-  <div
-    v-if="game ? getTypeAttribute < Boolean > (game, 'fast', false) : false"
-    class="row"
-    style="justify-content: space-between"
-  >
-    <button
-      v-for="i in [1, 2, 3]"
-      @click="selectedMultiplier = i"
-      :class="{
-        selected: selectedMultiplier == i,
-      }"
+  <template v-if="!forcedCompletion">
+    <div
+      v-if="game ? getTypeAttribute < Boolean > (game, 'fast', false) : false"
+      class="row"
+      style="justify-content: space-between"
     >
-      {{ multiplierToString(i) }}
-    </button>
-  </div>
-  <div class="row" style="height: 12em">
-    <button @click="registerMiss">&#10008;</button>
-    <button @click="registerHit">&#10004;</button>
-  </div>
-  <button @click="emit('undo')">&#x232B;</button>
+      <button
+        v-for="i in [1, 2, 3]"
+        @click="selectedMultiplier = i"
+        :class="{
+          selected: selectedMultiplier == i,
+        }"
+      >
+        {{ multiplierToString(i) }}
+      </button>
+    </div>
+    <div class="row" style="height: 12em">
+      <button @click="registerMiss">&#10008;</button>
+      <button @click="registerHit">&#10004;</button>
+    </div>
+    <button @click="emit('undo')">&#x232B;</button>
 
-  <button @click="emit('resign')">Resign</button>
+    <button @click="emit('resign')">Resign</button>
+  </template>
+
+  <template v-if="forcedCompletion">
+    <ForcedCompletion
+      :game="game"
+      :game-state="gameState"
+      @undo="emit('undo')"
+    ></ForcedCompletion>
+  </template>
 </template>
 
 <script lang="ts" setup>
@@ -33,7 +43,7 @@ import {
   getTypeAttribute,
   multiplierToString,
 } from '@/types/game'
-import { ref } from 'vue'
+import { computed, ref } from 'vue'
 
 const props = defineProps<{
   game: Game
@@ -47,6 +57,25 @@ const emit = defineEmits<{
   undo: []
   resign: []
 }>()
+
+const maxVisits = getTypeAttribute<number | null>(props.game, 'maxVisits', null)
+
+const forcedCompletion = computed(() => {
+  if (maxVisits === null) return false
+
+  const lastPlayerId = props.gameState.playersLeft.at(-1)
+  if (lastPlayerId == null) return false
+
+  const lastPlayerLeg = props.game.legs.find(
+    (leg) => leg.userId == lastPlayerId
+  )
+  const lastPlayerVisits = lastPlayerLeg?.visits ?? []
+
+  return (
+    lastPlayerVisits.length >= maxVisits &&
+    !lastPlayerVisits.at(-1)?.some((s) => s == null)
+  )
+})
 
 const getDefaultMultiplier = () =>
   props.game ? getTypeAttribute<Multiplier>(props.game, 'mode', 1) : 1
--- src/components/RtcOptionsInput.vue
+++ src/components/RtcOptionsInput.vue
@@ -53,6 +53,34 @@
     >
       Forced
     </button>
+    <div
+      style="
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        gap: 2em;
+      "
+    >
+      <div style="display: flex; gap: 1em">
+        <input
+          id="checkbox-max-visits"
+          type="checkbox"
+          v-model="maxVisitsEnabled"
+        />
+        <label style="margin: 0; font-weight: bold" for="checkbox-max-visits"
+          >Max Visits</label
+        >
+      </div>
+      <input
+        style="flex: 1"
+        :style="{ visibility: maxVisitsEnabled ? 'visible' : 'hidden' }"
+        type="number"
+        :min="3"
+        :max="1000"
+        :step="1"
+        v-model="maxVisits"
+      />
+    </div>
   </div>
 </template>
 
@@ -66,6 +94,10 @@ const random = ref(getTypeAttribute<boolean>(props, 'random', false))
 const forced = ref(getTypeAttribute<boolean>(props, 'forced', false))
 const fast = ref(getTypeAttribute<boolean>(props, 'fast', false))
 const mode = ref<1 | 2 | 3>(getTypeAttribute<1 | 2 | 3>(props, 'mode', 1))
+const maxVisits = ref<number>(getTypeAttribute<number>(props, 'maxVisits', 20))
+const maxVisitsEnabled = ref(
+  getTypeAttribute<number>(props, 'maxVisits', 0) > 0
+)
 
 const emit = defineEmits<{
   update: [typeAttributes: string[]]
@@ -80,6 +112,7 @@ const update = () => {
     `random:${random.value}`,
     `fast:${fast.value}`,
     `forced:${forced.value}`,
+    `maxVisits:${maxVisits.value}`,
   ])
 }
 
