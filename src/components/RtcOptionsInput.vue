c542ef065920d194ed9c1c6a92c2f94ecedc968b
Gregor Moe <moethegregor@gmail.com>
2025-12-29 14:50:09 +0100

    Puts maxVisits option into a div

--- src/components/RtcOptionsInput.vue
+++ src/components/RtcOptionsInput.vue
@@ -49,34 +49,34 @@
     >
       Forced
     </button>
-    <div
-      style="
-        display: flex;
-        align-items: center;
-        justify-content: space-between;
-        gap: 2em;
-      "
-    >
-      <div style="display: flex; gap: 1em">
-        <input
-          id="checkbox-max-visits"
-          type="checkbox"
-          v-model="maxVisitsEnabled"
-        />
-        <label style="margin: 0; font-weight: bold" for="checkbox-max-visits"
-          >Max Visits</label
-        >
-      </div>
+  </div>
+  <div
+    style="
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      gap: 2em;
+    "
+  >
+    <div style="display: flex; gap: 1em">
       <input
-        style="flex: 1"
-        :style="{ visibility: maxVisitsEnabled ? 'visible' : 'hidden' }"
-        type="number"
-        :min="3"
-        :max="1000"
-        :step="1"
-        v-model="maxVisits"
+        id="checkbox-max-visits"
+        type="checkbox"
+        v-model="maxVisitsEnabled"
       />
+      <label style="margin: 0; font-weight: bold" for="checkbox-max-visits"
+        >Max Visits</label
+      >
     </div>
+    <input
+      style="flex: 1"
+      :style="{ visibility: maxVisitsEnabled ? 'visible' : 'hidden' }"
+      type="number"
+      :min="3"
+      :max="1000"
+      :step="1"
+      v-model="maxVisits"
+    />
   </div>
 </template>
 
